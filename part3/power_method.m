3fcc25116729fcc5a0d06161edd9af429926744a
monamohebbi <monamohebbi9211@gmail.com>
2015-03-30 18:12:19 -0400

    removed and replaced debugging while statement with correct while statement

--- part3/power_method.m
+++ part3/power_method.m
@@ -2,12 +2,11 @@ function[lambda, u, numIters] = power_method(A, u, tol)
 numIters = 1;
 error = 1;
 lambda = 10;
-%while error > tol
-while numIters < 6
+while error > tol
     uNext = multiply_mat_vec(A, u)'
     lambda = uNext(1)
     uNext = uNext ./ lambda
-    error = minus(normal(uNext), u);
+    error = minus(uNext, u);
     error = sqrt(sum(error .^ 2))
     u = uNext 
     numIters = numIters + 1
