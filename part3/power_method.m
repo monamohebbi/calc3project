cc75b61df2f4a412111dcaeff7b1d9277bdcff7f
monamohebbi <monamohebbi9211@gmail.com>
2015-03-30 17:57:08 -0400

    corrected formulas for calculating eigenvalue and eigenvectors

--- part3/power_method.m
+++ part3/power_method.m
@@ -1,19 +1,15 @@
-function[lambda, v, numIters] = power_method(A, u, tol)
-numIters = 0
-error = 1
-lambda = 10
-while error > tol
+function[lambda, u, numIters] = power_method(A, u, tol)
+numIters = 1;
+error = 1;
+lambda = 10;
+%while error > tol
+while numIters < 6
     uNext = multiply_mat_vec(A, u)'
-    lambda = sqrt(sum(uNext .^ 2))
-    error = minus(uNext, u)
+    lambda = uNext(1)
+    uNext = uNext ./ lambda
+    error = minus(normal(uNext), u);
     error = sqrt(sum(error .^ 2))
-    u = normal(uNext)
+    u = uNext 
     numIters = numIters + 1
-    %error magnitude of uk+1 - uk <= tol
 end
-
-%while max error is greater than tolerance
-%recalculate error at the end of the 
-
-%replace with matrix read in from file
-%matA = [2 -12; 1 -5];
+end
