032eb9b50fa130d63707b9e5c4bd619576cb11b0
monamohebbi <monamohebbi9211@gmail.com>
2015-03-31 01:50:55 -0400

    wrote function to find largest value in vector, implemented it in power_method

--- part3/power_method.m
+++ part3/power_method.m
@@ -1,14 +1,18 @@
-function[lambda, u, numIters] = power_method(A, u, tol)
+function[lambda, numIters] = power_method(A, u, tol)
+
 numIters = 1;
 error = 1;
 lambda = 10;
-while error > tol
-    uNext = multiply_mat_vec(A, u)'
-    lambda = uNext(1)
-    uNext = uNext ./ lambda
+while error > tol && numIters <= 1000
+    uNext = multiply_mat_vec(A, u)';
+    lambda = largestValue(uNext);
+    uNext = uNext ./ lambda;
     error = minus(uNext, u);
-    error = sqrt(sum(error .^ 2))
-    u = uNext 
-    numIters = numIters + 1
+    error = sqrt(sum(error .^ 2));
+    u = uNext
+    numIters = numIters + 1;
+end
+if numIters > 1000
+    fprintf('Eigenvalues did not converge in 1000 iterations') 
 end
 end
